0ce2fcd7683a03fa0a026f434693cebf54dce8f0
Le Tuan Anh <anh.tuan.le@cern.ch>
2026-05-12 13:55:58 +0700

    add analysis file

---
+++ ana.C
@@ -0,0 +1,76 @@
+#include <TFile.h>
+#include <TTree.h>
+#include "TH1F.h"
+#include <iostream>
+#include <string>
+#include <map>
+
+void ana() {
+    // 1. Mở file ROOT
+    const char* filename = "Out.root";
+    TFile *file = TFile::Open(filename);
+    if (!file || file->IsZombie()) {
+        std::cerr << "Không thể mở file!" << std::endl;
+        return;
+    }
+
+    // 2. Lấy Tree
+    TTree *tree = (TTree*)file->Get("Products");
+    if (!tree) {
+        std::cerr << "Không tìm thấy Tree 'Products'!" << std::endl;
+        return;
+    }
+
+    // 3. Khai báo biến đọc dữ liệu (chỉ lấy các nhánh cần thiết)
+    char name[256];;
+    Int_t volumeID;
+    Double_t posZ;
+    tree->SetBranchAddress("name", name);
+    tree->SetBranchAddress("volumeID", &volumeID);
+    tree->SetBranchAddress("posZ", &posZ);
+
+    // 4. Khai báo MAP để thống kê
+    // Cấu trúc: map[volumeID][particle_name] = count
+    std::map<int, std::map<std::string, int>> volumeStats;
+
+    // 5. Duyệt Tree và đổ dữ liệu vào map
+    Long64_t nEntries = tree->GetEntries();
+    std::cout << "Đang xử lý " << nEntries << " entries..." << std::endl;
+    Double_t Z_vol5_offset = 2.0755;//mm
+    TH1F *hz0 = new TH1F("hZCu64","Yield of Cu-64 along the depth of Target chamber",100,0.,3.2);
+    for (Long64_t i = 0; i < nEntries; i++) {
+        tree->GetEntry(i);
+        
+        // Tăng số đếm cho hạt 'name' tại 'volumeID' tương ứng
+        // Nếu key chưa tồn tại, map sẽ tự khởi tạo giá trị là 0 rồi cộng thêm 1
+        std::string sname(name);
+        size_t pos = sname.find("[");
+        if (pos != std::string::npos) {
+            auto tmpname = sname.substr(0, pos);
+            if (tmpname == "Cu64") sname = tmpname;
+        }
+        volumeStats[volumeID][sname]++;
+        //std::cout<<volumeID<<std::endl;
+        if (volumeID == 5) {
+            auto ztemp = posZ - Z_vol5_offset;
+            hz0->Fill(ztemp);
+        }
+
+    }
+
+    // 6. In kết quả thống kê ra màn hình
+    std::cout << "\n--- KẾT QUẢ THỐNG KÊ ---" << std::endl;
+    std::cout << "VolumeID | Particle Name | Count" << std::endl;
+    std::cout << "--------------------------------" << std::endl;
+
+    for (auto const& [volID, particleMap] : volumeStats) {
+        for (auto const& [pName, count] : particleMap) {
+            std::printf("ID: %-5d | Name: %-10s | Count: %d\n", volID, pName.c_str(), count);
+        }
+        std::cout << "--------------------------------" << std::endl;
+    }
+
+    hz0->Draw();
+    // 7. Đóng file
+    //file->Close();
+}
